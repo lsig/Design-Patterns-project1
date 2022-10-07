3b7e8bc2479b4b86efd3f40c3788f02a75566016
Lsig <logis21@ru.is>
2022-10-07 13:06:56 +0000

    Logger_config_builder, added default build and returned self when function is called for better queue handling. Refactored as_sync and implemented add enviroment function in the builder

--- structured_logging/logger_creation/logger_config_builder.py
+++ structured_logging/logger_creation/logger_config_builder.py
@@ -4,35 +4,47 @@ from structured_logging.processors.I_processor import IProcessor
 from structured_logging.sinks.I_sink import ISink
 from structured_logging.sinks.console_sink import ConsoleSink
 from structured_logging.sinks.file_sink import FileSink
+from structured_logging.processors.environment_processor import EnvironmentProcessor
+from structured_logging.processors.null_processor import NullProcessor
 
 
 class LoggerConfigBuilder:
     def __init__(self):
         self._clear()
+        self.__logger.sink = self.with_console_sink()
+        self.__logger.processor = self.add_processor(NullProcessor())
+        self.__logger.is_async = False
+        self.__logger.async_wait_delay_in_seconds = 0
 
     def with_custom_sink(self, sink: ISink) -> 'LoggerConfigBuilder':
         self.__logger.sink = sink
+        return self
 
     def with_file_sink(self, file_path: str) -> 'LoggerConfigBuilder':
         self.__logger.sink = FileSink(file_path)
+        return self
 
     def with_console_sink(self) -> 'LoggerConfigBuilder':
         self.__logger.sink = ConsoleSink()
+        return self
 
     def as_async(self, wait_delay_in_seconds: int) -> 'LoggerConfigBuilder':
+        self.__logger.is_async = True
         self.__logger.async_wait_delay_in_seconds = wait_delay_in_seconds
+        return self
 
     def add_environment(self, environment: Environment) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
+        self.add_processor(EnvironmentProcessor(environment))
+        return self
 
     def add_processor(self, processor: IProcessor) -> 'LoggerConfigBuilder':
         self.__logger.processor = processor
+        return self
 
     def _clear(self):
         self.__logger = LoggerConfig()
 
     def build(self) -> LoggerConfig:
-        self._clear()
         return self.__logger
 
   
