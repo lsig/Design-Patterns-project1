8c54ead1ad21dda102508dc2b01cb3c9e83db9fc
Lsig <logis21@ru.is>
2022-10-08 13:12:17 +0000

    Container for structured logger is complete (testing needed)

--- structured_logging/infrastructure/container.py
+++ structured_logging/infrastructure/container.py
@@ -1,5 +1,17 @@
 from dependency_injector import containers, providers
+from structured_logging.command_queue.queue import Queue
+from structured_logging.configuration.logger_config import LoggerConfig
+from structured_logging.logger.logger import Logger
+from structured_logging.processors.null_processor import NullProcessor
+from structured_logging.sinks.console_sink import ConsoleSink
+
+
 
 
 class Container(containers.DeclarativeContainer):
-    raise NotImplementedError()
+        config: LoggerConfig = providers.Configuration()
+
+        logger = providers.Singleton(Logger,
+                                logger_config=config,
+                                logging_queue=Queue(config.async_wait_delay_in_seconds)
+                            )
