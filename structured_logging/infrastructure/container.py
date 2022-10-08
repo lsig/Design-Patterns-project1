a9997c5e9d60f4e3c3ebe1442b9ec655e01d42fc
Lsig <logis21@ru.is>
2022-10-08 13:17:05 +0000

    Queue provider made in case current container fails

--- structured_logging/infrastructure/container.py
+++ structured_logging/infrastructure/container.py
@@ -2,11 +2,6 @@ from dependency_injector import containers, providers
 from structured_logging.command_queue.queue import Queue
 from structured_logging.configuration.logger_config import LoggerConfig
 from structured_logging.logger.logger import Logger
-from structured_logging.processors.null_processor import NullProcessor
-from structured_logging.sinks.console_sink import ConsoleSink
-
-
-
 
 class Container(containers.DeclarativeContainer):
         config: LoggerConfig = providers.Configuration()
@@ -15,3 +10,5 @@ class Container(containers.DeclarativeContainer):
                                 logger_config=config,
                                 logging_queue=Queue(config.async_wait_delay_in_seconds)
                             )
+
+        __queue_provider = providers.Singleton(Queue, async_delay=config.async_wait_delay_in_seconds)
